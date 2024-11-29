308685c3631c1a64af410e3b8b50e0a5822f96e9
Eric Forbes <66398324+UnrestrictedTechnologies@users.noreply.github.com>
2024-11-29 14:32:51 -0500

    Initial Commit

---
+++ Reminders Application/Reminders_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_ApplicationApp.swift
+//  Reminders Application
+//
+//  Created by Eric Forbes on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
